bcb4bafc59732cbb9b2781211f411a62c101344a
harsha <harshaanamika97@gmail.com>
2022-04-14 15:51:26 +0530

    recent changes

--- resources/views/Frontend/Product/product_page.blade.php
+++ resources/views/Frontend/Product/product_page.blade.php
@@ -29,7 +29,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="shop-area pb-70 pb-lg-50 pb-md-40 pb-sm-60 pb-xs-50">
-                    <div class="row">
+                    {{-- <div class="row">
                         <div class="col-12">
                             <!-- Grid & List View Start -->
                             <div class="shop-topbar-wrapper d-flex justify-content-between align-items-center">
@@ -42,7 +42,7 @@
                             </div>
                             <!-- Grid & List View End -->
                         </div>
-                    </div>
+                    </div> --}}
                     <div class="row">
                         <div class="col-lg-3 order-lg-1 order-2">
                             <div class="common-sidebar-widget">
@@ -155,8 +155,8 @@
                 data :{
                     category_id: "{{ request()->id }}",
                     sub_id: "{{ request()->sub_id }}",
-                    fromrange: fromRange,
-                    torange: toRange
+                    fromrange: +fromRange,
+                    torange: +toRange
                 } ,
                 dataType: 'json',
                 success:function(data){
