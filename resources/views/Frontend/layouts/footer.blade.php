f833fbd2c9bcdec0322693bd2cb7aad60474c178
harsha <harshaanamika97@gmail.com>
2022-04-19 15:38:24 +0530

    recent changes

--- resources/views/Frontend/layouts/footer.blade.php
+++ resources/views/Frontend/layouts/footer.blade.php
@@ -15,8 +15,8 @@
                     <img src="{{ asset('assets/images/qr.jpg') }}" style="height:70px; width:70px"/></br>
                     <a href=" https://linktr.ee/" style="color:#b49f64">Follow Us!</a>
                     <div class="footer-social">
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/Goodmanloft"><i class="fa fa-facebook"></i></a>
+                        <a href="https://www.instagram.com/goodmansloft/"><i class="fa fa-instagram"></i></a>
 
                     </div>
                 </div>
@@ -40,8 +40,8 @@
                     <ul class="ft-menu">
                         <li><a href="{{ route('about.index') }}">About Us</a></li>
                         <li><a href="{{ route('privacy') }}">Privacy Policy</a></li>
-                        <li><a href="#">Shopping Guide</a></li>
-                        <li><a href="#">Delivery Information</a></li>
+                        <li><a href="{{ route('faq') }}">Shopping Guide</a></li>
+                        <li><a href="{{ route('faq') }}">Delivery Information</a></li>
                         <li><a href="{{ route('faq') }}">FAQ</a></li>
                         <li><a href="{{ route('term.condition') }}">Terms & Conditions</a></li>
 
